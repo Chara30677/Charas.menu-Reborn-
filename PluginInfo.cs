64bcad991ddb5b91d2f00aaeb50dc3dc5f8ec95c
Chara <154650379+Chara30677@users.noreply.github.com>
2025-08-24 12:08:36 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace CharaMenu
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.chara.gorillatag.charaMenu";
+        public const string Name = "Chara's Menu";
+        public const string Description = "Created by @chara10163 with love <3";
+        public const string Version = "1.0.0";
+    }
+}
